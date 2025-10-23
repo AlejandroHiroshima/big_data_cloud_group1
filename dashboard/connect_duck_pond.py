66d378fff17eec58b06cb2010de9d9986e619a4b
Erik Unevik <erikunevik@gmail.com>
2025-10-23 10:58:53 +0200

    fixing dashboard

--- dashboard/connect_duck_pond.py
+++ dashboard/connect_duck_pond.py
@@ -39,4 +39,5 @@ DB_PATH = os.getenv("DUCKDB_PATH")
 def query_job_listings(query: str):
     # query ska vara en komplett SELECT-sats
     with duckdb.connect(DB_PATH, read_only=True) as conn:
+        conn.execute("SET schema 'marts'")
         return conn.execute(query).df()
