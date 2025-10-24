8d23a376d0a1b634b49f831f1a5308690134927a
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-10-24 13:02:31 +0200

    changing yaml files and path to connect_duck_pond

--- dashboard/connect_duck_pond.py
+++ dashboard/connect_duck_pond.py
@@ -34,10 +34,10 @@ from pathlib import Path
 import os
 import duckdb
 
-DB_PATH = os.getenv("DUCKDB_PATH")
+FILES_SHARE_PATH = Path("/mnt/data/job_ads.duckdb")
 
 def query_job_listings(query: str):
     # query ska vara en komplett SELECT-sats
-    with duckdb.connect(DB_PATH, read_only=True) as conn:
+    with duckdb.connect(FILES_SHARE_PATH, read_only=True) as conn:
         conn.execute("SET schema 'marts'")
         return conn.execute(query).df()
