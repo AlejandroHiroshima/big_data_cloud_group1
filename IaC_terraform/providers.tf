e401bc03120d597a0ce3e4d7fe15a81761d81f94
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-10-30 10:58:17 +0100

    terraforming more!

--- IaC_terraform/providers.tf
+++ IaC_terraform/providers.tf
@@ -1,15 +1,33 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 4.4" 
+#     }
+#   }
+#   required_version = "~> 1.12"
+# }
+
+# provider "azurerm" {
+#   features {
+
+#   }
+# }
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.4" 
+      version = "~> 4.4"
     }
   }
-  required_version = "~> 1.12"
+  required_version = ">= 1.6"
 }
-
+ 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
+ 
+  subscription_id = "29a87bec-b199-436d-a319-1bc66a3a8c30" # byt ut mot din subscription ID
+  # tenant_id       = ""
 }
+
